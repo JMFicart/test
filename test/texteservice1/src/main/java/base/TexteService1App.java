ba55a604a526929e94569487f6519b0e13d66ff8
JMFicart <92925108+JMFicart@users.noreply.github.com>
2022-05-17 16:55:02 +0200

    Add files via upload

---
+++ test/texteservice1/src/main/java/base/TexteService1App.java
@@ -0,0 +1,11 @@
+package base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TexteService1App {
+    public static void main(String[] args) {
+        SpringApplication.run(TexteService1App.class, args);
+    }
+}
---
+++ test/texteservice2/src/main/java/base/TexteService2App.java
@@ -0,0 +1,11 @@
+package base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TexteService2App {
+    public static void main(String[] args) {
+        SpringApplication.run(TexteService2App.class, args);
+    }
+}
